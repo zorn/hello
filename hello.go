59bb8e24f69c6d6333cc2ae18d3461fd6ddeb167
Mike Zornek <mike@clickablebliss.com>
2014-11-12 20:47:56 -0500

    adding newmath lib

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/zorn/newmath"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+	fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
